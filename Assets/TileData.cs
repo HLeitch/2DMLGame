da236d1c05033998b76e670320dcfc4fd956abf8
HLeitch <55984810+HLeitch@users.noreply.github.com>
2022-01-05 16:01:12 +0000

    Tile refactor complete

---
+++ Assets/TileData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Tilemaps;
+
+public struct TileData
+{
+    public TileData(Tile tile, JumpingState jumpType,int numberOfJumps,int xPosition)
+    {
+        this.tile = tile;
+        this.jumpType = jumpType;
+        this.numberOfJumps = numberOfJumps;
+        this.xPosition = xPosition;
+    }
+
+    public Tile tile;
+    /// <summary>
+    /// Type of jump required to complete
+    /// </summary>
+    public JumpingState jumpType;
+    /// <summary>
+    /// jumps expected to complete
+    /// </summary>
+    public int numberOfJumps;
+    /// <summary>
+    /// position in tilemap
+    /// </summary>
+    public int xPosition;
+}
--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -3,67 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public struct TileData
-{
-    TileData(Tile tile, JumpingState jumpType,int numberOfJumps,int xPosition)
-    {
-        this.tile = tile;
-        this.jumpType = jumpType;
-        this.numberOfJumps = numberOfJumps;
-        this.xPosition = xPosition;
-    }
-
-    Tile tile;
-    /// <summary>
-    /// Type of jump required to complete
-    /// </summary>
-    JumpingState jumpType;
-    /// <summary>
-    /// jumps expected to complete
-    /// </summary>
-    int numberOfJumps;
-    /// <summary>
-    /// position in tilemap
-    /// </summary>
-    int xPosition;
-}
-
-
 public class TileManager : MonoBehaviour
 {
-    public List<List<Tile>> SingleJumpTileLists;
-    public List<List<Tile>> DoubleJumpTileLists;
-    public List<List<Tile>> DashJumpTileLists;
+    public List<List<Tile>> SingleJumpTileLists = new List<List<Tile>>();
+    public List<List<Tile>> DoubleJumpTileLists = new List<List<Tile>>();
+    public List<List<Tile>> DashJumpTileLists = new List<List<Tile>>() ;
+
+    Dictionary<List<List<Tile>>, JumpingState> TilelistToJumpState = new Dictionary<List<List<Tile>>, JumpingState>();
+
     public List<Tile> stepDownTiles;
     public Tile flatTile;
 
+    public List<Tile> stepUpTiles;
+
     [Header("Place Tiles Here. Sort by Jump type and number of Jumping 'events'")]
     [SerializeField]
-    private List<Tile> SingleJump_1Event;
+    private List<Tile> SingleJump_1Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> SingleJump_2Event;
+    private List<Tile> SingleJump_2Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> SingleJump_3Event;
+    private List<Tile> SingleJump_3Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> SingleJump_4Event;
+    private List<Tile> SingleJump_4Event = new List<Tile>();
 
     [SerializeField]
-    private List<Tile> DoubleJump_1Event;
+    private List<Tile> DoubleJump_1Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> DoubleJump_2Event;
+    private List<Tile> DoubleJump_2Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> DoubleJump_3Event;
+    private List<Tile> DoubleJump_3Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> DoubleJump_4Event;
+    private List<Tile> DoubleJump_4Event = new List<Tile>();
 
     [SerializeField]
-    private List<Tile> DashJump_1Event;
+    private List<Tile> DashJump_1Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> DashJump_2Event;
+    private List<Tile> DashJump_2Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> DashJump_3Event;
+    private List<Tile> DashJump_3Event = new List<Tile>();
     [SerializeField]
-    private List<Tile> DashJump_4Event;
+    private List<Tile> DashJump_4Event = new List<Tile>();
 
 
 
@@ -74,6 +53,9 @@ public class TileManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Helper Function. Assigns tile lists to the relevent master list and assigns the relevent jump state to the master list.
+    /// </summary>
     private void ListAssignment()
     {
         SingleJumpTileLists.Add(SingleJump_1Event);
@@ -90,10 +72,14 @@ public class TileManager : MonoBehaviour
         DashJumpTileLists.Add(DashJump_2Event);
         DashJumpTileLists.Add(DashJump_3Event);
         DashJumpTileLists.Add(DashJump_4Event);
+
+        TilelistToJumpState.Add(SingleJumpTileLists, JumpingState.SingleJump);
+        TilelistToJumpState.Add(DoubleJumpTileLists, JumpingState.DoubleJump);
+        TilelistToJumpState.Add(DashJumpTileLists, JumpingState.Dash);
     }
 
 
-    Tile GetTile(List<List<Tile>> tileListList, int tileSeed)
+    TileData GetTile(List<List<Tile>> tileListList, int tileSeed, int xPos)
     {
         int countOfTileList = 0;
         foreach (List<Tile> list in tileListList)
@@ -105,25 +91,42 @@ public class TileManager : MonoBehaviour
 
         if (countOfTileList == 0)
         {
-            return GetFlatTile();
+            return new TileData(GetFlatTile(), JumpingState.Grounded, 0, xPos);      
         }
 
         int tileToGet = tileSeed % countOfTileList;
 
+        //This +1 is the jump events of the tile returned.
+        int jumpEvents = 1;
+
+        //this marks where in the parent list the current list is. E.g. list 0 starts at 0, list 1 starts at 0 + list0.count
         int indexReached = 0;
         foreach (List<Tile> list in tileListList)
         {
-            if ((tileToGet < indexReached + list.Count) && tileToGet > indexReached)
+            
+            if ((tileToGet < indexReached + list.Count) && tileToGet >= indexReached)
             {
+                
                 int indexOfTile = (tileToGet - indexReached);
-                return list[indexOfTile];
-            }
 
+                //Prevents a list with no elements from being pulled from
+                if (list.Count > 0)
+                {
+                    //Finds desired jumping state of tile. This is passed into tile data for analysis of performance.
+                    JumpingState _jumpingState = JumpingState.Grounded;
+                    TilelistToJumpState.TryGetValue(tileListList, out _jumpingState);
+
+
+                    return new TileData(list[indexOfTile], _jumpingState, jumpEvents, xPos);
+                }
+            }
+            jumpEvents++;
+            indexReached += list.Count;
         }
 
         //Failure State
-        Debug.LogError("Tile Retrieval Failed. Flat Tile Returned!!");
-        return (GetFlatTile());
+        Debug.LogError($"Tile Retrieval Failed. Flat Tile Returned at X Tile {xPos}!!");
+        return (new TileData(GetFlatTile(), JumpingState.Grounded, 0, xPos));
     }
 
 
@@ -131,34 +134,72 @@ public class TileManager : MonoBehaviour
     {
         int tileToGet = tileSeed % tileList.Count;
 
-        return (tileList[tileToGet]);
+        Tile tileToPlace = (tileList[tileToGet]);
+        return tileToPlace;
+
     }
 
 
-    Tile GetStepUpTile(int tileSeed)
+    public TileData GetStepUpTile(int tileSeed, int xPos)
     {
-        int i = (tileSeed % stepUpTiles.Count);
-        return stepUpTiles[i];
+        int i = 0;
+        if (stepUpTiles.Count > 0)
+        {
+            i = (tileSeed % stepUpTiles.Count);
+        }
+        //produces a flat tile if no step up tiles are present
+        else
+        {
+            return GetFlatTileData(xPos);
+        }
+
+        Tile tilePlaced = stepUpTiles[i];
+
+        List<List<Tile>>[] availableTileLists = new List<List<Tile>>[] { SingleJumpTileLists, DoubleJumpTileLists, DashJumpTileLists };
+
+        foreach (List<List<Tile>> tileListList in availableTileLists)
+        {
+            //number of jumping events in the tile placed
+            int jumpingEvents = 1;
+            foreach (List<Tile> tileGroup in tileListList)
+            {
+                if (tileGroup.Contains(tilePlaced))
+                {
+                    JumpingState _jumpingState = JumpingState.Grounded;
+                    TilelistToJumpState.TryGetValue(tileListList, out _jumpingState);
+
+                    return new TileData(tilePlaced, _jumpingState, jumpingEvents, xPos);
+                }
+                jumpingEvents++;
+            }
+        }
+
+        //On failure
+        Debug.LogError("Step up tile Retrieval failed. Tiles may not be placed properly and ideal jump numbers will not be reliable.");
+        return new TileData(tilePlaced, JumpingState.Grounded, 0, xPos);
     }
 
-    Tile GetStepDownTile(int tileSeed)
+    public TileData GetStepDownTile(int tileSeed, int xPos)
     {
         int i = (tileSeed % stepDownTiles.Count);
-        return stepDownTiles[i];
+        return new TileData(stepDownTiles[i],JumpingState.Grounded,0,xPos);
     }
 
-    Tile GetDashJumpTile(int tileSeed)
+   public TileData GetDashJumpTile(int tileSeed, int xPos)
     {
-        int i = (tileSeed % dashJumpTiles.Count);
-        return dashJumpTiles[i];
+        return GetTile(DashJumpTileLists, tileSeed,xPos);
     }
-    Tile GetFlatTile()
+   public TileData GetFlatTileData(int xPos)
+    {
+        return new TileData(flatTile, JumpingState.Grounded, 0, xPos);
+    }
+    public Tile GetFlatTile()
     {
         return flatTile;
     }
-    Tile GetSingleJumpTile(int tileSeed)
+   public TileData GetSingleJumpTile(int tileSeed, int xPos)
     {
-        return singleJumpTiles[0];
+        return GetTile(SingleJumpTileLists, tileSeed, xPos);
     }
 
 
